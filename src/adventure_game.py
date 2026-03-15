0128933005a1999931be6ed718328084114fc49b
maikeburkard <burkard.maike@gmail.com>
2026-03-15 17:31:31 +0100

    solution for first step of adventure game

---
+++ src/adventure_game.py
@@ -0,0 +1,10 @@
+destiny = input("Möchtest du in den Wald oder in die Stadt gehen? ")
+
+if (destiny == "Wald"):
+    print("Du gehst in den Wald.")
+
+elif (destiny == "Stadt"):
+    print("Du gehst in die Stadt.")
+
+else:
+    print("Bitte schreibe Wald oder Stadt.")
